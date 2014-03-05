094ea1b7614441180766dcdd8d1712542d6d5f61
gilsondev <contato@gilsondev.com>
2014-03-05 15:17:36 -0300

    Insert domain of the staging environment

--- djprojtemplates/djprojtemplates/settings.py
+++ djprojtemplates/djprojtemplates/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = '&7#p&@3n$@bywi_p4+il)5$s@yny7t3&82bj!)+0mwg&(z!m27'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = TEMPLATE_DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['djprojectemplates.herokuapp.com']
+ALLOWED_HOSTS = ['djprojectemplates.herokuapp.com',
+                 'djprojecttemplate-staging.herokuapp.com']
 
 
 # Application definition
